02d90aff9600575a45b23fca7d464927a0a87af0
Jeremy Pollock <jpollock911@gmail.com>
2026-05-08 15:51:20 -0700

    chore(smart-search): scaffold directory structure

---
+++ src/main/smart-search/index.ts
@@ -0,0 +1,4 @@
+export { SmartSearchHandler } from './SmartSearchHandler';
+export { SynonymStore } from './SynonymStore';
+export { SemanticConfig } from './SemanticConfig';
+export { TrackerStore } from './TrackerStore';
